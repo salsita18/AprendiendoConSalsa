47d0b6e46265b552c075c04e2f8e09817a402a5a
Facundo Pascual <sasalcz@gmail.com>
2020-08-19 22:45:24 -0300

    Initial commit

---
+++ Nueva carpeta/AprendiendoConSalsa/AprendiendoConSalsa/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AprendiendoConSalsa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hola Mundo!
+
+            Console.WriteLine("Los saluda, salsita de Salsa Tecnologica");
+
+            Console.WriteLine("¿Como te llamas?");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Che, un gusto viejardo, que buen nombre: ");
+            Console.WriteLine(nombre);
+
+            Console.WriteLine("¿Y cual es tu edad?");
+            string edad = Console.ReadLine();
+
+            Console.WriteLine("Que copado tener: ");
+            Console.WriteLine(edad);
+
+            Console.ReadLine();
+        }
+    }
+}
